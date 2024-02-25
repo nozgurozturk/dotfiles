186f2afe0fbbbe453769376c358123b4944b96d9
Ozgur Ozturk <ozgur@nozgurozturk.com>
2024-02-25 15:58:08 +0100

    netrw update

--- nvim/lua/noo/init.lua
+++ nvim/lua/noo/init.lua
@@ -13,8 +13,8 @@ vim.filetype.add({
     }
 })
 
-vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0       -- disable banner
+vim.g.netrw_browse_split = 0 -- open in same window
 vim.g.netrw_keepdir = 0
 vim.g.netrw_winsize = 30
-vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
